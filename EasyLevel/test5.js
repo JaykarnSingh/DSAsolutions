d7b41ed0b6f2492a6fc71867ead001e9e4e80f19
jaykarn singh <55475521+JaykarnSingh@users.noreply.github.com>
2026-01-30 15:49:00 +0530

    largest and smallest number find in array

---
+++ EasyLevel/test5.js
@@ -0,0 +1,21 @@
+//largest and smallest number find in array
+
+
+let data=[4,1,10,2,33,9,80,0,231]
+let l=data[0];
+let s=data[0];
+
+for(let i=1;i<data.length;i++){
+       if(data[i]>l){
+        l=data[i];
+
+       }
+       if(data[i]<s){
+        s=data[i];
+       }
+}
+
+console.log(l)
+
+console.log(s)
+
